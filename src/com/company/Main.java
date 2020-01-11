9740408b3fe659c161c76c12d84c67c630abb39f
Slavon777777777 <Slavon777777777@gmail.com>
2020-01-11 17:32:37 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+	// write your code here
+    }
+
+
+    public static void sum(int a, int b){
+        int c = a + b;
+        System.out.println(c);
+    }
+
+    public int sum(int a, int b, int c){
+        int d = a + b + c;
+        return d;
+    }
+
+    public void sum(double a, double b){
+        double c = a + b;
+        System.out.println(c);
+    }
+}
